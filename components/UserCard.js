1ce30bfb16388e8ee7ef75ed298e3a5fd8cc6ad2
scripterin <bogdanro777@gmail.com>
2026-06-07 17:33:15 +0300

    Update UserCard.js

--- components/UserCard.js
+++ components/UserCard.js
@@ -20,7 +20,7 @@ export default function UserCard({ user, backTo = '/hub' }) {
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" width="14" height="14">
             <polyline points="15 18 9 12 15 6"/>
           </svg>
-          Înapoi la Hub
+          Înapoi
         </button>
       )}
 
